7e3dcc4b5c080daf07feb55247920284724da8f0
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-06-10 17:04:52 +0300

    Completed dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.getElementById("main").remove();
+
+const newHeader = document.createElement("h1");
+// const newHeader = H1;
+newHeader.id = "victory";
+newHeader.textContent = `Aswan is the champion`;
+
+document.body.append(newHeader);
